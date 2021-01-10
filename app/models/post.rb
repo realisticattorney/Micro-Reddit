27c2a8d4f44a46c29f3d1234bbe819cc0c5554cb
realisticattorney <g.axl.aquila@gmail.com>
2021-01-10 20:30:44 -0300

    fix more linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
--- db/migrate/20210110173522_remove_string_from_posts.rb
+++ db/migrate/20210110173522_remove_string_from_posts.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class RemoveStringFromPosts < ActiveRecord::Migration[6.1]
   def change
     remove_column :posts, :string, :string
